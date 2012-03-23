4fc09c8b34eeac822c409ee732a29d4c19dc2a2d
Andy Aylward <aaylward@gmail.com>
2012-03-22 20:44:35 -0400

    added findem

---
+++ findem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+output="bad.log"
+for url in `cat urls.csv`
+do
+	code=`curl -s -w "%{http_code}" $url -o /dev/null`
+	(test $code == 400 || test $code == 404) && (echo $url >> $output) 
+done
