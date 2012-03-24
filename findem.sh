ca460bfde4c57088105469c6f732139d55d2f0b6
Andy Aylward <aaylward@gmail.com>
2012-03-23 21:22:37 -0300

    Update findem.sh

--- findem.sh
+++ findem.sh
@@ -3,6 +3,6 @@
 output="bad.log"
 for url in `cat urls.csv`
 do
-    code=`curl -s -w "%{http_code}" $url -o /dev/null`
-    (test $code == 400 || test $code == 404) && (echo $url >> $output) 
+	code=`curl -s -w "%{http_code}" $url -o /dev/null`
+	(test $code == 400 || test $code == 404) && (echo $url >> $output) 
 done
