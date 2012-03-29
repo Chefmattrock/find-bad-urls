2f8b26704e09af53f7f0a24425ba3cdf25524286
Andy Aylward <aaylward@gmail.com>
2012-03-29 16:03:17 -0400

    it now accepts command line args

--- findem.sh
+++ findem.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 output="bad.log"
-for url in `cat urls.csv`
+echo "Starting Tests for $1..." >> $output
+counter=0
+for url in `cat $1`
 do
 	code=`curl -s -w "%{http_code}" $url -o /dev/null`
-	(test $code == 400 || test $code == 404) && (echo $url >> $output) 
+	(test $code == 400 || test $code == 404) && (echo $url >> $output && $counter += 1) 
 done
+echo "found $counter bad urls" | tee >> $output
+echo "found $counter bad urls"
