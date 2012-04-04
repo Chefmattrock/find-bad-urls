1a7b771cb9d213e8fc87c26f9d70268254a03531
Andy Aylward <aaylward@gmail.com>
2012-04-04 15:37:25 -0300

    curl should follow redirects

--- findem.sh
+++ findem.sh
@@ -5,7 +5,7 @@ echo "starting tests for $1 at `date -u +%s`000" >> $output
 counter=0
 for url in `cat $1`
 do
-	code=`curl -s -w "%{http_code}" $url -o /dev/null`
+	code=`curl -sL -w "%{http_code}" $url -o /dev/null`
 	(test $code == 400 || test $code == 404) && (echo $url >> $output && $counter += 1) 
 done
 echo "found $counter bad urls" | tee -a $output
