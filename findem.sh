a32cff7c40a70043139c6bae61b6573d14a71cc4
Andy Aylward <aaylward@gmail.com>
2012-04-06 16:18:17 -0400

    factored out some echo and tee stuff

--- findem.sh
+++ findem.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+log() {
+	echo "$@" | tee -a $output
+}
+
 if ! [ $1 ]; then
 	echo "usage: findem.sh <url_file> [output_file]"
 	exit
@@ -11,12 +15,12 @@ else
 	output="bad.log"
 fi
 
-echo "starting tests for $1 at `date -u +%s`000" | tee -a $output
+log "starting tests for $1 at `date -u +%s`000"
 counter=0
 for url in `cat $1`
 do
 	code=`curl -sL -w "%{http_code}" $url -o /dev/null`
 	(test $code == 400 || test $code == 404) && (echo $url >> $output && $counter += 1) 
 done
-echo "found $counter bad urls" | tee -a $output
+log "found $counter bad urls"
 
